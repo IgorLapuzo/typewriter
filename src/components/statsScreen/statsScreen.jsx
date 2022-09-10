fc7e09de1ffbb58bc84e653851b524e76c100ec8
IgorLapuzo <104383591+IgorLapuzo@users.noreply.github.com>
2022-09-10 11:01:41 +0300

    realized displaying statistic

--- src/components/statsScreen/statsScreen.jsx
+++ src/components/statsScreen/statsScreen.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
 import { getResults, getUserInfo } from '../../store/selectors';
 import { fetchResults } from '../../store/apiActions';
+import { Navigate } from 'react-router';
+import classNames from 'classnames';
+import Header from '../header/header';
+import styles from './statsScreen.module.scss';
+import { AppRoutes } from '../../constants';
+
 
 function StatsScreen () {
   const results = useSelector(getResults);
@@ -10,20 +15,34 @@ function StatsScreen () {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchResults(user));
-  }, [results]);
+    user.id && dispatch(fetchResults(user));
+  }, [user]);
+
+  if (!user.id) {
+    return <Navigate to={AppRoutes.ROOT} />;
+  }
 
   return (
     <React.Fragment>
-      <h1>Users result <span>{user.name}</span></h1>
-      <ol>
-        {results.map((res) => (
-          <li key={res.id}>
-            <p>Speed: <span>{res.speed}</span></p>
-            <p>Accuracy: <span>{res.precision}</span></p>
-          </li>
-        ))}
-      </ol>
+      <Header />
+      <main className={classNames('row', 'align-items-center')}>
+        <div className={classNames('row', 'justify-content-center', 'align-items-center')}>
+          <h1 className={styles.title}>User result <span>{user.name}</span></h1>
+          <div className={classNames('col-4', 'row', 'justify-content-center', 'align-items-center', styles.wrapper)}>
+
+            <ol >
+              {results.map((res, index) => (
+                <li key={`${res.id}${res.speed}`}>
+                  <p>Speed: <span className={styles.result}>{res.speed}</span></p>
+                  <p>Accuracy: <span className={styles.result}>{res.precision}</span></p>
+                  {index !== results.length - 1 && <hr/>}
+                </li>
+              ))}
+            </ol>
+
+          </div>
+        </div>
+      </main>
     </React.Fragment>
   );
 }
